d0fa9a77ac26fc6cac2a0416e2e8e477775acd76
Alabhya Jindal <aj@alabhya.me>
2025-07-15 08:06:52 +0100

    Binaryen mapping to print numbers

--- foo.ts
+++ foo.ts
@@ -12,13 +12,14 @@ module.addFunctionImport(
   binaryen.i32
 );
 
-let str = 'Hello, World!\n'
+let s = 55
+let str = String(s) + '\n' 
 let strArr = new TextEncoder().encode(str)
 module.setMemory(1, 2, 'memory', [{offset: module.i32.const(66), data: strArr}]);
 
 let instrs = [
 module.i32.store(0, 4, module.i32.const(0), module.i32.const(66)),
-module.i32.store(0, 4, module.i32.const(4), module.i32.const(14)),
+module.i32.store(0, 4, module.i32.const(4), module.i32.const(strArr.length)),
 
 
 module.drop(
@@ -37,7 +38,7 @@ module.addFunctionExport('_start', '_start');
 module.validate();
 
 let wat = module.emitText()
-console.log(wat)
+// console.log(wat)
 Bun.write('build/temp.wat', wat)
 
 await $`wasmtime build/temp.wat`
