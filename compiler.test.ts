76d56fdb64a00b4ddef6ec6be342fa570afe240b
Alabhya Jindal <aj@alabhya.me>
2025-07-17 19:24:44 +0100

    Compare expected with result

--- compiler.test.ts
+++ compiler.test.ts
@@ -1,5 +1,5 @@
 import { readdir } from 'node:fs/promises'
-import { test } from 'bun:test'
+import { expect, test } from 'bun:test'
 import scan from './src/scanner'
 import parse from './src/parser'
 import compile from './src/compiler'
@@ -12,12 +12,14 @@ for (const fileName of fileNames) {
   test(`can compile ${fileName}`, async () => {
     let sourceText = await Bun.file(`${testDir}/${fileName}`).text()
 
+    let expected = sourceText.split('\n')[0]!.substring(2).trim()
+
     let tokens = scan(sourceText)
     let statements = parse(tokens)
     let wat = compile(statements)
 
-    let res = await $`bun build.ts`.text()
-    // read the first commend from the file - that is what the stdout should be compared against
-    console.log('yoo', res)
+    let out = await $`bun build.ts`.text()
+    console.log([...out].length)
+    expect(out).toBe(expected)
   })
 }
