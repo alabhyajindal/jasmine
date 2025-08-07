d53c30b649065a4695f18e91c5613e06aadfd9f2
Alabhya Jindal <aj@alabhya.me>
2025-08-07 23:45:09 +0100

    Compile based on the provided backend

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { $ } from 'bun'
 import scan from './scanner'
 import parse from './parser'
-import compile from './compiler'
+import compile from './binaryen'
 // @ts-ignore Do not look for type declarations for the source language
 import source from '../__.jas'
 
@@ -9,14 +9,15 @@ let _ = source
 
 const args = Bun.argv.slice(2)
 
-const validCall = args.length == 3 && args[1] == '--target' && args[2] == 'binaryen'
-const filePath = args[0]!
-const targetValue = args[2]
+const validCall = args.length == 3 && args[1] == '--backend' && args[2] == 'binaryen'
 
 if (!validCall) {
-  console.log('Usage: bun compile <file.jas> --target binaryen')
+  console.log('Usage: bun compile <file.jas> --backend <binaryen | qbe>')
   process.exit()
 }
+
+const filePath = args[0]!
+const backend = args[2]!
 
 const sourceFile = Bun.file(filePath)
 if (!(await sourceFile.exists())) {
@@ -36,9 +37,13 @@ async function run(source: string) {
   // console.log(JSON.stringify(statements, null, 2))
   // return
 
-  const wat = compile(statements)
-  if (!wat) reportError('Compilation failed.')
+  if (backend == 'binaryen') {
+    const wat = compile(statements)
+    if (!wat) reportError('Compilation failed.')
 
-  Bun.write('build/main.wat', wat)
-  await $`wasm-merge build/main.wat main lib/utils.wasm utils -o build/a.wasm --enable-multimemory`
+    Bun.write('build/main.wat', wat)
+    await $`wasm-merge build/main.wat main lib/utils.wasm utils -o build/a.wasm --enable-multimemory`
+  } else if (backend == 'qbe') {
+    // do something
+  }
 }
