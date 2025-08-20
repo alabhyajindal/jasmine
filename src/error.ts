a31735ad877d99aa9ede5f5f7f72a86bed1992c2
Alabhya Jindal <aj@alabhya.me>
2025-08-20 20:07:13 +0100

    Add comment

--- src/error.ts
+++ src/error.ts
@@ -8,5 +8,6 @@ export function reportError(msg: string, token?: Token): never {
     console.log(msg)
 
     if (process.env.TESTING) throw 'CompilerError'
+    // Commenting the following line results in a 100% code coverage, the following line can never really be tested by the tests
     process.exit(1)
 }
